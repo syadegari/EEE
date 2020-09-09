68c50035d4909231d28fa18aa743d0052f551c0c
Sourena Yadegari Varnamkhasti <sourena.yadegari@ufz.de>
2020-09-09 08:42:18 +0200

    mask object for mpr and htessel

---
+++ examples/mpr-htessel/mpr-htessel-aux/mask.py
@@ -0,0 +1,139 @@
+from typing import Dict, List
+import f90nml as nml
+from abstract_namelist import AbstractNameList
+
+
+def warn_after_first_call(msg):
+    def f(func):
+        setattr(func, 'counter', 0)
+
+        def g(*args, **kwargs):
+            func.counter += 1
+            if func.counter > 1:
+                print(msg)
+            return func(*args, **kwargs)
+
+        return g
+
+    return f
+
+
+def is_valid_range(list_two):
+    return len(list_two) == 2 and list_two[0] <= list_two[1]
+
+
+def is_list(list_two):
+    return type(list_two) == list
+
+
+class ParamMask:
+
+    def __init__(self, maskfile: str, namelist: AbstractNameList, default_threshold=0.2) -> None:
+        self.maskfile = maskfile
+        self.namelist = namelist
+        self.mask_dict = self.read_mask()
+        self.default_threshold = default_threshold
+
+    def read_mask(self) -> Dict:
+        try:
+            return self.read_mask_generic()
+        except FileNotFoundError:
+            param_list = self.namelist.get_all_model_parameters()
+            return {k: {'min': None, 'max': None} for k in param_list}
+
+    def read_mask_generic(self) -> Dict:
+        """
+        Reads the mask file and returns a dictionary containing
+         the names of parameters that will be used in sensitivity analysis:
+        {name: str -> {'min': float/None, 'max': float/None}}
+        If default is indicated, 'min' and 'max' are set to None, otherwise to
+        their specified values.
+
+        Returns
+        -------
+
+        """
+        nmlist = nml.read(self.maskfile)
+        d = {}
+        for section in nmlist.keys():
+            for subsection in nmlist[section]:
+                if is_list(nmlist[section][subsection]) and is_valid_range(nmlist[section][subsection]):
+                    d[subsection] = {'min': nmlist[section][subsection][0],
+                                     'max': nmlist[section][subsection][1]}
+                elif nmlist[section][subsection].lower() == 'default':
+                    d[subsection] = {'min': None, 'max': None}
+                else:
+                    raise Exception(
+                        f'key {subsection} in file {self.maskfile} does not have the correct format')
+        return d
+
+    def fill_none_values(self) -> None:
+        """
+        if 'min' and 'max' have None values, fill these keys with
+        the default_value found in the namelist file (plus/minus) threshold/2
+
+        Returns
+        -------
+
+        """
+        for par_name in self.mask_dict:
+            if self.mask_dict[par_name]['min'] == None:
+                default_value = self.namelist[par_name]
+                if default_value >= 0:
+                    self.mask_dict[par_name]['min'] = (1. - self.default_threshold / 2) * default_value
+                    self.mask_dict[par_name]['max'] = (1. + self.default_threshold / 2) * default_value
+                else:
+                    self.mask_dict[par_name]['min'] = (1. + self.default_threshold / 2) * default_value
+                    self.mask_dict[par_name]['max'] = (1. - self.default_threshold / 2) * default_value
+
+    def fill_default_values(self) -> None:
+        """
+        adds the default value to the dictionary of each parameter.
+        after this, the dictionary contains all the information needed
+        for the parameters.dat file:
+
+        name : {'min': min_val, 'max': max_val 'default': default_val},
+
+        where the default value is found in the namelist file
+
+        Returns
+        -------
+
+        """
+        for par_name in self.mask_dict:
+            default_value = self.namelist[par_name]
+            self.mask_dict[par_name]['default'] = default_value
+
+    def _lower_bound_eq_default(self, param):
+        if self.mask_dict[param]['min'] == self.mask_dict[param]['default']:
+            print(f'min and default for parameter {param} are the same. Consider readjusting the values')
+
+    def _upper_bound_eq_default(self, param):
+        if self.mask_dict[param]['max'] == self.mask_dict[param]['default']:
+            print(f'max and default for parameter {param} are the same. Consider readjusting the values')
+
+    def _all_eq(self, param):
+        if self.mask_dict[param]['min'] == self.mask_dict[param]['max'] == self.mask_dict[param]['default']:
+            print(f'min, max and default for parameter {param} are the same. Consider readjusting the values')
+
+    def check_values(self):
+        for param in self.mask_dict:
+            assert self.mask_dict[param]['min'] <= self.mask_dict[param]['default'] <= self.mask_dict[param]['max']
+            self._all_eq(param)
+            self._lower_bound_eq_default(param)
+            self._upper_bound_eq_default(param)
+
+    # @warn_after_first_call('Warning: .get_mask method is called more than once')
+    def get_mask(self) -> Dict[str, Dict[str, float]]:
+        """
+        Gets the complete dictionary containing all the information needed for
+        parameters.dat file. This is the main method of the class.
+
+        Returns
+        -------
+
+        """
+        self.fill_none_values()
+        self.fill_default_values()
+        self.check_values()
+        return self.mask_dict
